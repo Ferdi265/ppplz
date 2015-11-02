2616900dc2f68eec71716a42fd48d073dc0352ca
theFerdi265 <theferdi265@gmail.com>
2015-11-02 12:54:56 +0100

    Add mod formatting, + minor format changes

--- fmt.js
+++ fmt.js
@@ -2,13 +2,87 @@
 
 const osu = require('./osu.js');
 
+const fmtmods = (score) => {
+	const mods = {
+		Hidden: 8,
+		HardRock: 16,
+		SuddenDeath: 32,
+		Perfect: 16384,
+		DoubleTime: 64,
+		NightCore: 512,
+		FlashLight: 1024,
+		FadeIn: 1048576,
+		Easy: 2,
+		NoFail: 1,
+		HalfTime: 256,
+		SpunOut: 4096,
+		Key4: 32768,
+		Key5: 65536,
+		Key6: 131072,
+		Key7: 262144,
+		Key8: 524288,
+	};
+	const selected = score.mods;
+
+	let modStr = '';
+
+	if (
+		selected & mods.Hidden ||
+		selected & mods.HardRock ||
+		selected & mods.DoubleTime ||
+		selected & mods.NightCore ||
+		selected & mods.SuddenDeath ||
+		selected & mods.Perfect ||
+		selected & mods.FlashLight ||
+		selected & mods.FadeIn
+	) {
+		modStr += '+';
+		if (selected & mods.Hidden) modStr += 'HD';
+		if (selected & mods.HardRock) modStr += 'HR';
+		if (selected & mods.NightCore) {
+			modStr += 'NC';
+		} else if (selected & mods.DoubleTime) {
+			modStr += 'DT';
+		}
+		if (selected & mods.Perfect) {
+			modStr += 'PF';
+		} else if (selected & mods.SuddenDeath) {
+			modStr += 'SD';
+		}
+		if (selected & mods.FlashLight) modStr += 'FL';
+		if (selected & mods.FadeIn) modStr += 'FI';
+	}
+	if (
+		selected & mods.Easy ||
+		selected & mods.NoFail ||
+		selected & mods.HalfTime ||
+		selected & mods.SpunOut ||
+		selected & mods.Key4 ||
+		selected & mods.Key5 ||
+		selected & mods.Key6 ||
+		selected & mods.Key7 ||
+		selected & mods.Key8
+	) {
+		modStr += '-';
+		if (selected & mods.Easy) modStr += 'EZ';
+		if (selected & mods.NoFail) modStr += 'NF';
+		if (selected & mods.HalfTime) modStr += 'HT';
+		if (selected & mods.SpunOut) modStr += 'SO';
+		if (selected & mods.Key4) modStr += 'K4';
+		if (selected & mods.Key5) modStr += 'K5';
+		if (selected & mods.Key6) modStr += 'K6';
+		if (selected & mods.Key7) modStr += 'K7';
+		if (selected & mods.Key8) modStr += 'K8';
+	}
+	return modStr;
+};
 const fmtpp = (user, recent, beatmap, score, bestrank, userdata) => {
 		return (
 		(score === undefined ? '' : 'New PB! ') +
 		(bestrank === undefined ? '' : '#' + bestrank + ' of your top plays! ') +
-		'Achieved ' + recent.rating + ' on [https://osu.ppy.sh/b/' + beatmap.id + ' ' + beatmap.artist + ' - ' + beatmap.title + ' [' + beatmap.difficultyName + ']]. ' +
-		(score === undefined ? '' : 'Raw PP: ' + score.pp.toFixed(2) + '.') +
-		(bestrank === undefined ? '' : ' Weighted PP: ' + (score.pp * Math.pow(0.95, bestrank - 1)).toFixed(2) + '.') +
+		'Achieved ' + recent.rating + ' on [https://osu.ppy.sh/b/' + beatmap.id + ' ' + beatmap.artist + ' - ' + beatmap.title + ' [' + beatmap.difficultyName + ']' + fmtmods(recent) + ']. ' +
+		(score === undefined ? '' : 'Raw PP: ' + score.pp.toFixed(2) + '. ') +
+		(bestrank === undefined ? '' : 'Weighted PP: ' + (score.pp * Math.pow(0.95, bestrank - 1)).toFixed(2) + '. ') +
 		fmtuser(user, recent, beatmap, score, bestrank, userdata) 
 	);
 };
@@ -33,7 +107,7 @@ const fmtpbs = (user, recent, beatmap, score, bestrank, userdata) => {
 const fmtuser = (user, recent, beatmap, score, bestrank, userdata) => {
 	let relPP = user.pp - (userdata.pp || user.pp);
 	let relRank = user.rank - (userdata.rank || user.rank);
-	return (relPP === 0 && relRank === 0 ? '' : ' ' +
+	return (relPP === 0 && relRank === 0 ? '' :
 		(relPP > 0 ? 'Gained ' + relPP.toFixed(2) + ' pp!' : 'Lost ' + (-1 * relPP).toFixed(2) + ' pp.') + ' ' +
 		(relRank < 0 ? 'Rose ' + (-1 * relRank) + ' ranks!' : 'Fell ' + relRank + ' ranks.')
 	);
